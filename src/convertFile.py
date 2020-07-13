f25e4403b7acf386c6a0cc21c792d4f0a60c8546
marcos <marcos@localhost.localdomain>
2020-07-13 03:03:30 -0300

    novo testeUnitario_CSV_JSON

--- src/convertFile.py
+++ src/convertFile.py
@@ -1,19 +1,23 @@
 import csv, requests
 
-arquivoCSV = "../TC_001_PaginaPrincipal_02_07_2020_13_27.csv"
-url = "http://localhost:8080/api/testefuncional"
+def testFuncional_CSV_JSON():
+    arquivoCSV = "../TC_001_PaginaPrincipal_02_07_2020_13_27.csv"
+    url = "http://localhost:8080/api/testefuncional"
 
-try:
-    with open (arquivoCSV) as csvFile:
-        id =1
-        csvReader = csv.DictReader(csvFile)
-        statusAPI = requests.get(url)
+    try:
+        with open (arquivoCSV) as csvFile:
+            id =1
+            csvReader = csv.DictReader(csvFile)
+            statusAPI = requests.get(url)
 
-        while statusAPI.status_code == "200":
-            for csvRow in csvReader:
-                dic = {'id':id,'status':csvRow['status'],'data':csvRow['data'],'hora':csvRow['hora'],'mtodo':csvRow['metodo'],'mensagem':csvRow['mensagem']}
-                requests.post(url, json=dic)
-                id+=1
-    
-except Exception as e:
-    print(e)
+            while statusAPI.status_code == "200":
+                for csvRow in csvReader:
+                    dic = {'id':id,'status':csvRow['status'],'data':csvRow['data'],'hora':csvRow['hora'],'mtodo':csvRow['metodo'],'mensagem':csvRow['mensagem']}
+                    requests.post(url, json=dic)
+                    id+=1
+
+    except Exception as e:
+        print(e)
+
+def testeUnitario_CSV_JSON():
+    arquivoCSV = "../TC_001_PaginaPrincipal_02_07_2020_13_27.csv"
