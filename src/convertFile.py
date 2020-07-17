d53f72de06887dafc0999bdd9615e2987af46193
marcos <marcos@localhost.localdomain>
2020-07-17 00:16:52 -0300

    testeUnitario_TXT_JSON

--- src/convertFile.py
+++ src/convertFile.py
@@ -1,9 +1,12 @@
 import csv, requests
-import io
+#import io
+
+url_api_testFuncional = "http://localhost:8080/api/testefuncional"
+url_api_testUnitario = "http://localhost:8080/api/testeUnitario"
 
 def testFuncional_CSV_JSON():
     arquivoCSV = "../TC_001_PaginaPrincipal_02_07_2020_13_27.csv"
-    url_api_testFuncional = "http://localhost:8080/api/testefuncional"
+
 
     try:
         with open (arquivoCSV) as csvFile:
@@ -20,18 +23,49 @@ def testFuncional_CSV_JSON():
     except Exception as e:
         print(e)
 
-def testeUnitario_TXT_JSON(palavra):
+def testeUnitario_TXT_JSON():
     try:
         arquivoTXT = "../log_maven_teste.txt"
         nun_linha = 0
-        result = []
+        test_run = []
+        list_dic = []
         with open(arquivoTXT, 'r') as arq:
             for linha in arq:
                 nun_linha += 1
-                if palavra in linha:
-                    result.append((linha.rstrip()))
-        return result
+                if "Tests run:" in linha:
+                    test_run.append((linha.strip()))
+        list_1 = test_run[1]
+        list_item = list_1.split(',')
+        for n in list_item:
+            list_dic.append(n.strip())
+
+        with open(arquivoTXT, 'r') as arq:
+            for linha in arq:
+                nun_linha += 1
+                if "Total time:" in linha:
+                    total_time = linha.rstrip()
+        list_dic.append(total_time[7:].strip())
+
+        with open(arquivoTXT, 'r') as arq:
+            for linha in arq:
+                nun_linha += 1
+                if "Finished at:" in linha:
+                    finished_at = linha.rstrip()
+        list_dic.append(finished_at[7:].strip())
+        remove_skipped = list_dic.pop(3)
+
+        data = list_dic[4][12:23]
+        hora = list_dic[4][24:32]
+        teste_ok = int(list_dic[0][18:].strip()) - int(list_dic[1][10:].strip())
+
+        dic = {'id': 1,'total_teste': list_dic[0][18:].strip(), 'passou':teste_ok,\
+               'erro': list_dic[2][8:].strip(), 'tempo_total': list_dic[3][11:19].strip(), \
+               'data': data.strip(), 'hora': hora.strip()}
+
+        print(dic)
+        requests.post(url_api_testUnitario, json=dic)
+
     except Exception as e:
         print(e)
 
-print(testeUnitario_TXT_JSON("Tests run:"))
+print(testeUnitario_TXT_JSON())
